44c2528d370c31cccc56afb3d1851480dbdf3665
gracelove <govlmo91@gmail.com>
2020-01-29 21:32:14 +0900

    테스트

---
+++ src/test/java/who/gracelove/demospringboot/sample/SampleControllerTest2.java
@@ -0,0 +1,29 @@
+package who.gracelove.demospringboot.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SampleControllerTest2 {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @MockBean
+    SampleService mockSampleService;
+
+    @Test
+    void hello() {
+        when(mockSampleService.getName()).thenReturn("gracelove");
+
+        String result = testRestTemplate.getForObject("/hello", String.class);
+        assertThat(result).isEqualTo("hello gracelove");
+    }
+
+}
---
+++ src/test/java/who/gracelove/demospringboot/sample/SampleControllerTest3.java
@@ -0,0 +1,31 @@
+package who.gracelove.demospringboot.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SampleControllerTest3 {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockBean
+    SampleService mockSampleService;
+
+    @Test
+    void hello() {
+        when(mockSampleService.getName()).thenReturn("gracelove");
+
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello gracelove");
+    }
+
+}
