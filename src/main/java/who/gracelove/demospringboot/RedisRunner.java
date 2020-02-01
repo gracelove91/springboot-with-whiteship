7f4d7a6c04e93f7edaaf54c5121e926129e46871
gracelove <govlmo91@gmail.com>
2020-02-01 14:26:22 +0900

    스프링 데이터 REDIS

--- src/main/java/who/gracelove/demospringboot/RedisRunner.java
+++ src/main/java/who/gracelove/demospringboot/RedisRunner.java
@@ -6,6 +6,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
+import who.gracelove.demospringboot.account.Account;
+import who.gracelove.demospringboot.account.AccountRepository;
+
+import java.util.Optional;
 
 @Component
 public class RedisRunner implements ApplicationRunner {
@@ -13,10 +17,23 @@ public class RedisRunner implements ApplicationRunner {
     @Autowired
     StringRedisTemplate redisTemplate;
 
+    @Autowired
+    AccountRepository accountRepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         ValueOperations<String, String> values = redisTemplate.opsForValue();
         values.set("eunmo", "grace");
         values.set("springboot", "2.2");
+
+        Account account = new Account();
+        account.setUsername("grace");
+        account.setEmail("govlmo91@gmail.com");
+
+        accountRepository.save(account);
+
+        Optional<Account> byId = accountRepository.findById(account.getId());
+        System.out.println("username = " + byId.get().getUsername());
+        System.out.println("email = " + byId.get().getEmail());
     }
 }
