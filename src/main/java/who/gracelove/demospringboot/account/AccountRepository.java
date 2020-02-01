1fa6e15eb04a4f03ed3702085f3d1a60d5ab2b98
gracelove <govlmo91@gmail.com>
2020-02-01 14:16:25 +0900

    스프링 데이터 REDIS

--- src/main/java/who/gracelove/demospringboot/account/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package who.gracelove.demospringboot.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findByUsername(String username);
-}
